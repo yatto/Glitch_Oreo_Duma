a21c9b6dd811511082b9638e79464b72df369bd2
yatto <yattodettaman@hotmail.com>
2017-06-06 17:19:50 +0200

    return to msm8960

--- release/build/magic/initgen.sh
+++ release/build/magic/initgen.sh
@@ -63,12 +63,12 @@ null="abc"
 
 #Permissive Selinux
 if [ "$PERMISSIVE" == "1" ]; then
-  echo "cmdline = console=ttyHSL0,115200,n8 androidboot.hardware=flo user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 vmalloc=340M enforcing=0 androidboot.selinux=permissive" $l2_opt $vdd_uv $null >> /tmp/cmdline.cfg
+  echo "cmdline = console=ttyHSL0,115200,n8 androidboot.hardware=msm8960 user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 vmalloc=340M enforcing=0 androidboot.selinux=permissive" $l2_opt $vdd_uv $null >> /tmp/cmdline.cfg
 else
 <<<<<<< HEAD
-  echo "cmdline = console=ttyHSL0,115200,n8 androidboot.hardware=flo user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 vmalloc=340M enforcing=1 androidboot.selinux=enforcing" $l2_opt $vdd_uv $null >> /tmp/cmdline.cfg
+  echo "cmdline = console=ttyHSL0,115200,n8 androidboot.hardware=msm8960 user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 vmalloc=340M enforcing=1 androidboot.selinux=enforcing" $l2_opt $vdd_uv $null >> /tmp/cmdline.cfg
 =======
-  echo "cmdline = console=ttyHSL0,115200,n8 androidboot.hardware=flo user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 vmalloc=340M" $l2_opt $vdd_uv $null >> /tmp/cmdline.cfg
+  echo "cmdline = console=ttyHSL0,115200,n8 androidboot.hardware=msm8960 user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 vmalloc=340M" $l2_opt $vdd_uv $null >> /tmp/cmdline.cfg
 >>>>>>> bd53eb9557c81858d1b1ade67efe7af57c0b8bab
 fi
 
